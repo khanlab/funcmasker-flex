f0763cf0229d2200abf7fc5a01b755db42586e38
Ali Khan <alik@robarts.ca>
2022-05-19 10:53:35 -0400

    fix typo

--- funcmasker_flex/run.py
+++ funcmasker_flex/run.py
@@ -7,7 +7,7 @@ from snakebids.cli import add_dynamic_args
 
 def get_parser():
     """Exposes parser for sphinx doc generation, cwd is the docs dir"""
-    app = SnakeBidsApp("../funcmasker-flex")
+    app = SnakeBidsApp("../funcmasker_flex")
     add_dynamic_args(app.parser, app.config["parse_args"], app.config["pybids_inputs"])
     return app.parser
 
