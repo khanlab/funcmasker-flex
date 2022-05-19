0b9351f4e83a984823eac438032f6668d951a28f
Ali Khan <alik@robarts.ca>
2022-05-19 10:47:18 -0400

    fix for docs dynamic args

--- funcmasker_flex/run.py
+++ funcmasker_flex/run.py
@@ -2,12 +2,15 @@
 from pathlib import Path
 
 from snakebids.app import SnakeBidsApp
+from snakebids.cli import add_dynamic_args
 
 
 def get_parser():
     """Exposes parser for sphinx doc generation, cwd is the docs dir"""
-    app = SnakeBidsApp("../")
+    app = SnakeBidsApp("../funcmasker-flex")
+    add_dynamic_args(app.parser, app.config["parse_args"], app.config["pybids_inputs"])
     return app.parser
+
 
 
 def main():
